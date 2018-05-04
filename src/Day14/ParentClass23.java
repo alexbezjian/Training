056f84394e9a12ac3519b991397bfb0fe6eb5ab6
alexb <alexbezjian@gmail.com>
2018-05-04 18:11:33 +0400

    association Aggregation impl HW (missing file)

---
+++ src/Day14/ParentClass23.java
@@ -0,0 +1,21 @@
+package Day14;
+
+public class ParentClass23 {
+    public String super2;
+    public int sdd= 3;
+
+    public void display() {
+        System.out.println("ssew");
+    }
+
+    public ParentClass23(String super2) {
+        this.super2 = super2;
+        System.out.println("Parent " + super2);
+    }
+
+    public ParentClass23() {
+        System.out.println("call from parent");
+
+    }
+
+}
